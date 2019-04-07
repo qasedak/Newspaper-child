d8fee577a80f73d4968628353f18bf5d583f6c8d
Mohammad Anbarestany <m_anbarestany@hotmail.com>
2019-04-07 23:14:36 +0430

    Initial commit

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+/*  ----------------------------------------------------------------------------
+    Newspaper V9.0+ Child theme - Please do not use this child theme with older versions of Newspaper Theme
+
+    What can be overwritten via the child theme:
+     - everything from /parts folder
+     - all the loops (loop.php loop-single-1.php) etc
+	 - please read the child theme documentation: http://forum.tagdiv.com/the-child-theme-support-tutorial/
+
+
+     - the rest of the theme has to be modified via the theme api:
+       http://forum.tagdiv.com/the-theme-api/
+
+ */
+
+
+
+
+/*  ----------------------------------------------------------------------------
+    add the parent style + style.css from this folder
+ */
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 1001);
+function theme_enqueue_styles() {
+    wp_enqueue_style('td-theme', get_template_directory_uri() . '/style.css', '', TD_THEME_VERSION, 'all' );
+    wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/style.css', array('td-theme'), TD_THEME_VERSION . 'c', 'all' );
+    wp_enqueue_style('td-theme-child', get_stylesheet_directory_uri() . '/rtl.css', array('td-theme'), TD_THEME_VERSION . 'c', 'all' );
+
+}
